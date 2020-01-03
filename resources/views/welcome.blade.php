0d9b4a1d9811fa0c2f26246ca6934b03005ae104
Marco Nogueira <marconogueira.senai@gmail.com>
2020-01-03 13:41:23 -0300

    Test changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    FreeCodeGram
+                    FreeCodeGram2
                 </div>
 
                 <div class="links">
